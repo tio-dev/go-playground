a083a03ae30eb67c2e39ca9d3cf55c1ad6db26c4
Richard Anchieta <richard.anchieta@stemativa.com.br>
2022-07-01 11:14:10 -0300

    wip: adiciona template (sem apexcharts)

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,15 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require('@nrwl/next/plugins/with-nx');
+const path = require('path')
 
-/**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
- **/
-const nextConfig = {
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  experimental: {
+    esmExternals: false,
+    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   },
-};
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
 
-module.exports = withNx(nextConfig);
+    return config
+  }
+}
---
+++ apps/app/src/@core/components/react-apexcharts/index.tsx
@@ -0,0 +1,15 @@
+// ** Next Import
+import dynamic from 'next/dynamic'
+
+// ! To avoid 'Window is not defined' error
+//const ReactApexcharts = dynamic(() => import('react-apexcharts'), { ssr: false })
+
+//export default ReactApexcharts
+
+const Chart = () => {
+  return (
+    <div>chart</div>
+  )
+}
+
+export default Chart
